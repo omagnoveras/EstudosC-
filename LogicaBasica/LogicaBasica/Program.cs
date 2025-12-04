604782bc9f5318557f330a03bd9f5ae21050031b
Magno Veras - Alado <magno.veras88@gmail.com>
2025-12-04 15:30:13 -0300

    Exercício 1

--- LogicaBasica/LogicaBasica/Program.cs
+++ LogicaBasica/LogicaBasica/Program.cs
@@ -6,33 +6,44 @@ namespace LogicaBasica
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("=== CÁLCULO DE VALOR DE PEÇAS ===");
-            Console.WriteLine();
-            Console.Write("Digite o código da peça 1: ");
-            int codigoPeca1 = int.Parse(Console.ReadLine()!);
+            string continuar = "sim";
 
-            Console.Write("Digite a quantidade de peças 1: ");
-            int quantidadePeca1 = int.Parse(Console.ReadLine()!);
+            while (continuar != "sair")
+            {
+                Console.WriteLine("=== CÁLCULO DE VALOR DE PEÇAS ===");
+                Console.WriteLine();
+                Console.Write("Digite o código da peça 1: ");
+                int codigoPeca1 = int.Parse(Console.ReadLine()!);
 
-            Console.Write("Digite o valor unitário da peça 1: ");
-            double valorUnitarioPeca1 = double.Parse(Console.ReadLine()!);
+                Console.Write("Digite a quantidade de peças 1: ");
+                int quantidadePeca1 = int.Parse(Console.ReadLine()!);
 
-            Console.Write("Digite o código da peça 2: ");
-            int codigoPeca2 = int.Parse(Console.ReadLine()!);
+                Console.Write("Digite o valor unitário da peça 1: ");
+                double valorUnitarioPeca1 = double.Parse(Console.ReadLine()!);
 
-            Console.Write("Digite a quantidade de peças 2: ");
-            int quantidadePeca2 = int.Parse(Console.ReadLine()!);
+                Console.Write("Digite o código da peça 2: ");
+                int codigoPeca2 = int.Parse(Console.ReadLine()!);
 
-            Console.Write("Digite o valor unitário da peça 2: ");
-            double valorUnitarioPeca2 = double.Parse(Console.ReadLine()!);
+                Console.Write("Digite a quantidade de peças 2: ");
+                int quantidadePeca2 = int.Parse(Console.ReadLine()!);
 
-            double valorTotalPeca1 = quantidadePeca1 * valorUnitarioPeca1;
-            double valorTotalPeca2 = quantidadePeca2 * valorUnitarioPeca2;
+                Console.Write("Digite o valor unitário da peça 2: ");
+                double valorUnitarioPeca2 = double.Parse(Console.ReadLine()!);
 
-            double valorAPagar = valorTotalPeca1 + valorTotalPeca2;
+                double valorTotalPeca1 = quantidadePeca1 * valorUnitarioPeca1;
+                double valorTotalPeca2 = quantidadePeca2 * valorUnitarioPeca2;
 
-            Console.WriteLine($"VALOR A PAGAR: R$ {valorAPagar:F2}");
-            Console.ReadKey();
+                double valorAPagar = valorTotalPeca1 + valorTotalPeca2;
+
+
+                Console.WriteLine($"VALOR A PAGAR: R$ {valorAPagar:F2}");
+                Console.WriteLine();
+                Console.WriteLine("Digite 'sair' para encerrar ou pressione ENTER para continuar...");
+                continuar = Console.ReadLine()!.ToLower();
+                Console.Clear();
+
+            }
+            Console.WriteLine("Programa Encerrado!");
         }
 
     }
